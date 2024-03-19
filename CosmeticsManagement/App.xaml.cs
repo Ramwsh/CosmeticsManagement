7b018d7a2405913c41839f75e3f97381950429f4
Не Скажу <jimkrauz@gmail.com>
2024-03-20 00:04:53 +0300

    Main Window update

--- CosmeticsManagement/App.xaml.cs
+++ CosmeticsManagement/App.xaml.cs
@@ -3,14 +3,6 @@
 namespace CosmeticsManagement
 {    
     public partial class App : Application
-    {
-        protected override void OnStartup(StartupEventArgs e)
-        {
-            TestForBrand.Execute();
-            TestForDelivery.Execute();
-            TestForFactory.Execute();
-            TestForOrder.Execute();
-            TestForProduct.Execute();
-        }
+    {        
     }
 }
--- CosmeticsManagement/MainWindow.xaml.cs
+++ CosmeticsManagement/MainWindow.xaml.cs
@@ -1,19 +1,26 @@
 ﻿using System.Windows;
+using System.Windows.Input;
 
 namespace CosmeticsManagement
 {    
     public partial class MainWindow : Window
     {
-
-
         public MainWindow()
         {
             InitializeComponent();
+        }
 
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Environment.Exit(0);
+        }
 
-
-
-
+        private void Window_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            if (Mouse.LeftButton == MouseButtonState.Pressed)
+            {
+                DragMove();
+            }
         }
     }
 }
